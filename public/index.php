a631b5b4114619c37ce5dde04dd255a79560d78a
Wesley Willians <wesleywillians@gmail.com>
2019-04-16 20:41:15 -0400

    Corrigindo bug de dar vergonha ao vivo

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 $date = new \DateTime;
 $date->setDate(1984,9,8);
-$today = new \DataTime;
+$today = new \DateTime;
 
 $age = new \App\Age($date, $today);
 ?>
-Minha idade é: <?=$age;?>
+Minha idade é: <?=$age->calculate();?>
