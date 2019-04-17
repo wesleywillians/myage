3ad25ca86e71392db87e7b76f2f9af5d6290e3b0
Wesley Willians <wesleywillians@gmail.com>
2019-04-16 20:37:46 -0400

    Exibindo minha idade

--- public/index.php
+++ public/index.php
@@ -1,1 +1,11 @@
-Hello
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$date = new \DateTime;
+$date->setDate(1984,9,8);
+$today = new \DataTime;
+
+$age = new \App\Age($date, $today);
+?>
+Minha idade é: <?=$age;?>
